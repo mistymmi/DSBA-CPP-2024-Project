6ff59edd623b6e7dbc185899f9cb9962d21f201b
helvetinlivet <Helvetinlivet.work@gmail.com>
2024-06-02 15:10:31 +0300

    Update main.cpp with export function

--- main.cpp
+++ main.cpp
@@ -5,9 +5,22 @@
 #include "dbscan.h"
 #include "kmeans.h"
 
-int main() {
-    std::vector<Point> points = readCSV("filepath.csv");
+void exportClusters(const std::vector<Point>& points, const std::string& filename) {
+    std::ofstream file(filename);
+    if (file.is_open()) {
+        file << "x,y,cluster\n";
+        for (const auto& point : points) {
+            file << point.x << "," << point.y << "," << point.cluster << "\n";
+        }
+        file.close();
+    } else {
+        std::cerr << "Unable to open file for writing: " << filename << std::endl;
+    }
+}
 
+int main() {
+    std::vector<Point> points = readCSV("file.csv");
+    normalizePoints(points);
     int k = 3;
     kmeans(points, k);
     for (const auto& point : points) {
@@ -16,8 +29,10 @@ int main() {
     }
 
     drawClusters(points);
+    exportClusters(points, "kmeans_data.csv");
 
-    std::vector<Point> points1 = readCSV("filepath.csv");
+    std::vector<Point> points1 = readCSV("file.csv");
+    normalizePoints(points1);
     auto [bestEps, bestMinPts] = autoTuneDBSCAN(points1);
     dbscan(points1, bestEps, bestMinPts);
     for (const auto& point : points1) {
@@ -26,6 +41,7 @@ int main() {
     }
 
     drawClusters(points1);
+    exportClusters(points1, "dbscan_data.csv");
 
     return 0;
 }
